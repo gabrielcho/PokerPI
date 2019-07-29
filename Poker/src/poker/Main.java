3e5294c5f6032c70bacf7d8f316eb74046b4d19c
Gabriel Arango <gabrielarango15@gmail.com>
2019-07-29 18:24:58 -0500

    Se anadieron las imagenes

--- Poker/src/poker/Main.java
+++ Poker/src/poker/Main.java
@@ -2,9 +2,7 @@ package poker;
 
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-			//lololo
+	public static void main(String[] args) { //lololo
 	}
 
 }
